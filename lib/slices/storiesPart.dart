e7af5faa0e9dc637f7130857576e2a77d0987fde
toghrul <toghrul@toghruls-MacBook-Pro.local>
2025-06-25 20:25:18 +0400

    stories part done

--- lib/slices/storiesPart.dart
+++ lib/slices/storiesPart.dart
@@ -6,44 +6,41 @@ class StoriesPart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 200,
+      height: 180,
       child: ListView(
         scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.all(20),
         children: [
           ...List.generate(10, (index) {
-            return Padding(
-              padding: const EdgeInsets.all(10),
-              child: SizedBox(
-                height: 170,
-                width: 120,
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.network(
-                        'https://picsum.photos/200/300?random=$index',
-                        width: 120,
-                        height: 170,
-                        fit: BoxFit.cover,
-                      ),
+            return SizedBox(
+              height: 170,
+              width: 180,
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      'https://picsum.photos/200/300?random=$index',
+                      fit: BoxFit.cover,
+                      width: 140,
+                      height: 170,
                     ),
-
-                    Positioned(
-                      bottom: 15,
-                      left: 8,
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    left: 5,
+                    child: CircleAvatar(
+                      radius: 17,
+                      backgroundColor: Colors.white,
                       child: CircleAvatar(
-                        radius: 20,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 17,
-                          backgroundImage: NetworkImage(
-                            'https://i.pravatar.cc/150?img=$index',
-                          ),
+                        radius: 15,
+                        backgroundImage: NetworkImage(
+                          'https://picsum.photos/200/300?random=$index',
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             );
           }),
